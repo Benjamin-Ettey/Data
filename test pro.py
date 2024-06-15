21f53081547969eef932970d2b0c79dd00b3198e
Benjamin-Ettey <benjaminettey2004@gmail.com>
2024-06-14 18:04:16 -0700

    Add files via upload

---
+++ test pro.py
@@ -0,0 +1,8 @@
+print('Hello Customer, welcome to Zeepay')
+x=int(input('enter your user card number'))
+print('1:User Balance',' 2:Withdraw Money', '3:Request for loan','4:Help')
+b=int(input('select option'))
+x=int(input('Enter your pin'))
+if x==1:
+        print('Dear Customer, your balance is GHC 100.00')
+print('Press 1 for Okay') 
